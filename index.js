5a89e328ccf104fb792c527cc151687afa8f2c63
alexislopezdev <alexis.lopez201863@yahoo.com>
2022-08-27 23:57:12 -0300

    node practice update

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ let user = {
   name: "jose",
   surName: "ramirez",
 };
-console.log(`hola ${name}`);
-console.log(`hola soy ${user.name} y mi apellido es ${user.surName}`);
+//console.log(`hola ${name}`);
+//console.log(`hola soy ${user.name} y mi apellido es ${user.surName}`);
 
+let names = ["luna", "maria", "jose"];
 
-let names = ["luna","maria","jose"];
-
-for (let i = 0; i < names.length; i++){
-    console.log(`Hola ${names[i]}`);
+for (let i = 0; i < names.length; i++) {
+  //console.log(`Hola ${names[i]}`);
 }
+
+function employee(name, surname) {
+  return `the employee ${name} have the surname ${surname} `;
+}
+
+console.log(employee("Alexis", "lopez"));
+console.log(employee("Ariel", "Benitez"));
