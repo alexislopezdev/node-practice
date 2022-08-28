4abcca38763339e5433ecad469840e771d809c22
alexislopezdev <alexis.lopez201863@yahoo.com>
2022-08-27 22:58:15 -0300

    node practice

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = "alexis";
+let surName = "lopez";
+
+let user = {
+  name: "jose",
+  surName: "ramirez",
+};
+console.log(`hola ${name}`);
+console.log(`hola soy ${user.name} y mi apellido es ${user.surName}`);
+
+
+let names = ["luna","maria","jose"];
+
+for (let i = 0; i < names.length; i++){
+    console.log(`Hola ${names[i]}`);
+}
